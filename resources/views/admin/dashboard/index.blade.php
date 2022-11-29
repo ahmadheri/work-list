e059ba180d5b3e4b3e0db915c5e7a14e2b2b38de
ahmadheri <dev.ahmadheri@gmail.com>
2022-11-29 13:41:05 +0800

    add admin and  dashboard interface

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app', ['title' => 'Dashboard'])
+
+@section('content')
+<div class="container-fluid">
+    <!-- Page Heading -->
+
+
+    <!-- Content Row -->
+
+    <div class="row">
+
+        <!-- Area Chart -->
+        <div class="col-md-4">
+
+        </div>
+
+        <div class="col-md-4">
+          
+        </div>
+
+        <div class="col-md-4">
+          
+        </div>
+
+    </div>
+
+    <div class="row">
+
+    </div>
+</div>
+@endsection
