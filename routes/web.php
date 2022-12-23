b9f484b726f2e338122d7a75dd169f1be838475a
ahmadheri <dev.ahmadheri@gmail.com>
2022-12-23 14:48:23 +0800

    add middleware auth and Task Route

--- routes/web.php
+++ routes/web.php
@@ -19,28 +19,42 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
-})->name('login');
+});
 
 Route::prefix('admin')->group(function () {
-  Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+  Route::group(['middleware' => 'auth'], function() {
 
-  // User 
-  Route::get('/user', [UserController::class, 'index'])->name('admin.user.index');
-  Route::get('/user/create', [UserController::class, 'create'])->name('admin.user.create');
-  Route::post('/user/store', [UserController::class, 'store'])->name('admin.user.store');
-  Route::get('/user/edit/{user}', [UserController::class, 'edit'])->name('admin.user.edit');
-  Route::put('/user/update/{user}', [UserController::class, 'update'])->name('admin.user.update');
-  Route::delete('/user/{id}', [UserController::class, 'destroy']);
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+  
+    // User 
+    Route::get('/user', [UserController::class, 'index'])->name('admin.user.index');
+    Route::get('/user/create', [UserController::class, 'create'])->name('admin.user.create');
+    Route::post('/user/store', [UserController::class, 'store'])->name('admin.user.store');
+    Route::get('/user/edit/{user}', [UserController::class, 'edit'])->name('admin.user.edit');
+    Route::put('/user/update/{user}', [UserController::class, 'update'])->name('admin.user.update');
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
+  
+    // Customer
+    Route::get('/customer', [CustomerController::class, 'index'])->name('admin.customer.index');
+    Route::get('/customer/create', [CustomerController::class, 'create'])->name('admin.customer.create');
+    Route::post('/customer/store', [CustomerController::class, 'store'])->name('admin.customer.store');
+    Route::get('/customer/edit/{customer}', [CustomerController::class, 'edit'])->name('admin.customer.edit');
+    Route::put('/customer/update/{customer}', [CustomerController::class, 'update'])->name('admin.customer.update');
+    Route::delete('/customer/{id}', [CustomerController::class, 'destroy']);
+  
+    // Task
+    Route::get('/task', [TaskController::class, 'index'])->name('admin.task.index');
+    Route::get('/task/create', [TaskController::class, 'create'])->name('admin.task.create');
+    Route::post('/task/store', [TaskController::class, 'store'])->name('admin.task.store');
+    Route::get('/task/search-customer', [TaskController::class, 'searchCustomer'])->name('admin.task.search-customer');
+    Route::get('/task/search-customer/{id}', [TaskController::class, 'searchCustomerID']);
+    Route::get('/task/search-pic', [TaskController::class, 'searchPersonInCharge'])->name('admin.task.search-pic');
+    Route::get('/task/search-pic/{id}', [TaskController::class, 'searchPICID']);
+  
+    Route::get('/task/session', [TaskController::class, 'accessSessionData']);
 
-  // Customer
-  Route::get('/customer', [CustomerController::class, 'index'])->name('admin.customer.index');
-  Route::get('/customer/create', [CustomerController::class, 'create'])->name('admin.customer.create');
-  Route::post('/customer/store', [CustomerController::class, 'store'])->name('admin.customer.store');
-  Route::get('/customer/edit/{customer}', [CustomerController::class, 'edit'])->name('admin.customer.edit');
-  Route::put('/customer/update/{customer}', [CustomerController::class, 'update'])->name('admin.customer.update');
-  Route::delete('/customer/{id}', [CustomerController::class, 'destroy']);
+  });
 
-  // Task
-  Route::get('/task', [TaskController::class, 'index'])->name('admin.task.index');
 });
 
